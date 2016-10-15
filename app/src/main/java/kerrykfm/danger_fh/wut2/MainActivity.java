8b45e9fa4ce680a9a8b1b6b53f766c743a6159d0
DaNGeR <danger8201@gmail.com>
2016-10-15 15:01:19 +0700

    Finish first Day

--- app/src/main/java/kerrykfm/danger_fh/wut2/MainActivity.java
+++ app/src/main/java/kerrykfm/danger_fh/wut2/MainActivity.java
@@ -1,13 +1,49 @@
 package kerrykfm.danger_fh.wut2;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit การประกาศตัวแปร
+    //Access Type Name
+
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.livTraffic);
+        button = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                //getBaseContext() ภายใน
+                //getApplicationContext() ภายนอก
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cat);
+                mediaPlayer.start();
+
+                //Show WebView
+                String url = "https://youtu.be/n8X9_MgEdCg";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+
+            } // onClick
+        });
+
+    } // Main Method
+} // Main Class นี่คือ คลาสหลัก
